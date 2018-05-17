31e370b18112c574068081816bcea472e09aa423
Allen <b03901014@ntu.edu.tw>
2018-05-17 16:50:27 +0800

    Modify client to display the image from Tessel2

--- client/src/App.js
+++ client/src/App.js
@@ -3,15 +3,40 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      screenshot: null,
+    };
+    this._capture = this._capture.bind(this)
+	}
+  _capture() {
+    fetch('http://192.168.1.198:8888/', {
+      method: 'GET',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+      },
+    })
+    .then((response) => response.json())
+    .then(responseJson   => {
+        this.setState({screenshot: responseJson.result })
+    })
+  };
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Welcome to NTUEE ESLab HW2 Demo</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          {this.state.screenshot ?
+            <img id="result"
+                 width="640" height="480"
+                 src={this.state.screenshot} /> : null}
+
         </p>
       </div>
     );
